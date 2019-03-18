f81786e488b95a124807489454891e6a48b22c45
Farah <mijameskac@gmail.com>
2019-03-18 00:29:34 -0400

    Updated PyPoll Code

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -5,15 +5,21 @@ import csv
 election_file = os.path.join('Resources', 'election_data.csv')
 
 # initializing empty list to put in the separate list based on column headers
-voter= []
-county = []
-candidate = []
 
-#import the file path as a csvfile and delimit by commas
-with open(election_file, newline = '') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ",")
-   
-    
+election = {} 
+voter = []
+county = []
+candidate2 = []
+candidate = []
+#import the file path as a csvfile and delimit by commas 
+with open(election_file, ) as csv_file:
+    csvreader = csv.reader(csv_file, delimiter = ",")
+    csvreader = csv.DictReader(csv_file, delimiter = ",") # using the DictReader can reference using header name 
+    for line in csvreader:
+        if line["Candidate"] in election: # is candidate in dictionary
+            election[line["Candidate"]] = election[line["Candidate"]] + 1
+        else:
+            election[line["Candidate"]] = 1
     for row in csvreader:
         voter.append(row[0])
         county.append(row[1])
@@ -21,19 +27,33 @@ with open(election_file, newline = '') as csvfile:
     # remove the headers from lists, 
     voter = voter[1:]
     county = county[1:]
-    candidate = candidate[1:]
+    candidate2 = candidate[1:]
 
-total_votes = len(voter)
+#total_votes = len(voter)
+#print(total_votes)
+
+total_votes = sum(election.values())
+
+print(total_votes)
 
 # looked up how to extract unique values from list, set was most recommended 
 candidate2 = set(candidate)
 candidate2 =list(candidate)
 #finding the total number each candidate won by looping through if statement and initializing counts 
 
+# the dictionary keys contain the list of the candidates who got votes 
+print(election.keys())
+
+candidates = list(election.keys())
+candidates
+#finding the total number each candidate won by looping through if statement and
+#initializing counts 
+
 Khan = 0
 OTooley = 0
 Correy = 0
 Li =  0
+
 for i in candidate2:
     if i == "Khan":
         Khan += 1
@@ -43,6 +63,14 @@ for i in candidate2:
         Correy += 1
     else:
         Li += 1
+
+#checking accuracy         
+print(Li)
+print(Khan)
+print(OTooley)
+print(Correy)
+
+
 # finding the percentage of each candidate by dividng the total number by "total_votes"
 
 Khan_pct = round((Khan/total_votes)*100)
@@ -51,6 +79,17 @@ Correy_pct = round((Correy/total_votes)*100)
 Li_pct = round((Li/total_votes)*100)
 
 print_stmt = (f"Election Results \n--------------------\nTotal Votes: {total_votes} \n--------------------\n {candidate[0]}: {Khan_pct}00% ({Khan}) \n {candidate[1]}: {OTooley_pct}00% ({OTooley}) \n {candidate[2]}:{Correy_pct}00% ({Correy})  \n {candidate[3]}: {Li_pct}00% ({Li}) \n--------------------\n Winner: \n--------------------\n")
+print(print_stmt)
+# all_candidates = [Khan, OTooley, Correy, Li]
+# max(all_candidates)
+
+winner = max(election, key=election.get)
+
+# max(election.values())
+
+
+print_stmt = (f"Election Results \n--------------------\n Total Votes: {total_votes} \n--------------------\n {candidates[0]}:  {Khan_pct}.00% ({Khan}) \n {candidates[1]}: {Correy_pct}.00% ({Correy}) \n {candidates[2]}: {Li_pct}.00% ({Li}) \n {candidates[3]}: {OTooley_pct}.00% ({OTooley}) \n--------------------\n Winner: {winner}\n--------------------\n")
+
 print(print_stmt)
 
 
